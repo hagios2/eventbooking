ea3586cb1065993fde4a518cace377ffbebec692
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2022-09-21 22:24:02 +0000

    setting up types and resolvers

---
+++ app.js
@@ -0,0 +1,64 @@
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
+import { buildSchema } from 'graphql'
+
+const app = express()
+
+const events = []
+
+app.use(express.json())
+
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput!): Event!
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return events
+        },
+        createEvent: (args) => {
+            const { title, description, price, date } = args
+        
+            const event = {
+                _id: Math.random().toString(),
+                title,
+                description,
+                price: +price,
+                date: new Date().toISOString()
+            }
+
+            events.push(event)
+        }
+    },
+    graphiql: true
+}))
+
+const PORT  = process.env.PORT || 3100
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
